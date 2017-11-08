17204faa8f50d9875a493e9092cb931b2dafcbf5
nampdn <nampdn@protonmail.com>
2017-11-08 13:04:50 +0700

    chore(demo): rename `mock` folder to `demo`
    move index.js file to demo and update package.json `npm start` command

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,8 @@
-const samuel = require('./lib/prophet-samuel');
+const samuel = require('../lib/prophet-samuel');
 const path = require('path');
 
 samuel()
-  .watch(path.join(__dirname, 'mocks'), { recursive: true })
+  .watch(path.join(__dirname), { recursive: true })
   .subscribe(payload => {
     console.log(JSON.stringify(payload, null, 2), new Date().getTime());
   });
